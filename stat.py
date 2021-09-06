f6d40a431e937df1121e0514c4ac8dcbecc32986
Nikita <davidovnv@gmail.com>
2021-09-06 17:02:07 +0300

    Data 2 1. Dropped completly nan columns and objects. 2. Dropped feats with nan more than 0.925. 3. Num feats: KNN imputation. 4. Cat feats 1: MNAR - created new feats, nan to 'rare'. 5. Cat feats 2: MCAR - most frequent strategy.

--- stat.py
+++ stat.py
@@ -3,12 +3,36 @@ import pandas as pd
 
 dct = {}
 models = [
-          'rf: be, stnd', 'lgbm: be, stnd', 'rf', 'lgbm: ohe_t10, stnd', 
-           'sgd: ohe_t10, stnd', 'knn: ohe_t10, stnd', 'svm: ohe_t10, stnd',
-           'rf: le (max_val)', 'lgbm: le (max_val)',
-           'rf: le(max_val), stnd', 'lgbm: le (max val), stnd',
-          ]
-
+    'd1 rf: SS, be; rus', 
+    'd1 lgbm: SS, be; rus', 
+    'd1 rf; rus', 
+    'd1 lgbm: SS, ohe_top10; rus', 
+    'd1 sgd: SS, ohe_top10; rus', 
+    'd1 knn: SS, ohe_top10; rus', 
+    'd1 svm: SS, ohe_top10; rus', 
+    'd1 rf: le (max_val); rus', 
+    'd1 lgbm: le (max_val); rus', 
+    'd1 rf: SS, le(max_val); rus', 
+    'd1 lgbm: SS, le (max val); rus', 
+    'd1 rf: SS, ohe_top10; rus', 
+    'd2 lgbm: SS, ohe_top10; rus', 
+    'd2 sgd: SS, ohe_top10; rus', 
+    'd2 knn: SS, ohe_top10; rus', 
+    'd2 svm: SS, ohe_top10; rus', 
+    'd2 rf: be; rus', 
+    'd2 lgbm: be; rus',
+    'd2 rf: le(max_val); rus',    
+    'd2 lgbm: le(max_val); rus',
+    'd2 rf: be; rus',
+    'd2 lgbm: be; rus',
+    'd2 rf: le(max_val); rus',    
+    'd2 lgbm: le(max_val); rus',
+    'd2 rf: SS, be; rus',
+    'd2 lgbm: SS, be; rus',    
+    'd2 rf: SS, le(max_val); rus',    
+    'd2 lgbm: SS, le(max_val); rus',  
+]
+#%%
 lst = '''
 roc_auc  recall     prec      acc
 0.58908  0.5624  0.10638  0.61176
@@ -22,7 +46,27 @@ roc_auc  recall     prec      acc
 0.59462  0.62442  0.10464  0.56934
 0.597   0.576  0.109  0.614
 0.586   0.593  0.103  0.580
+0.60648  0.58380  0.11376  0.62574
+0.62616  0.62444  0.12016  0.62766
+0.57578  0.65038  0.09982  0.51244
+0.54188  0.57996  0.08690  0.50952
+0.60778  0.65638  0.10832  0.56650
+0.60134  0.58862  0.11042  0.61212
+0.62074  0.65444  0.11432  0.59216
+0.61326  0.63212  0.11282  0.59726
+0.61344  0.66012  0.11206  0.57378
+0.60134  0.58862  0.11042  0.61212
+0.62074  0.65444  0.11432  0.59216
+0.613   0.632  0.113  0.598
+0.613   0.660  0.112  0.574
+0.60780  0.60214  0.11254  0.61264
+0.61534  0.64284  0.11280  0.59200
+0.613   0.632  0.113  0.598
+0.618   0.659  0.114  0.583
 '''
+#%% 
+print(lst.split('\t'))
+#%%
 split = lst.split('\n')
 index = split[1].split()
 
@@ -33,4 +77,5 @@ for model, line in zip(models, split[2:]):
 
 df = pd.DataFrame(dct, index=index)
 df = df.T.sort_values(by='roc_auc', ascending=False).round(3)
+df.sort_index(inplace=True)
 print(df) 
