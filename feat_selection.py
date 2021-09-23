8617c966b8fbac4c964ca4da08dc1841f42ed621
Nikita <davidovnv@gmail.com>
2021-09-23 13:02:12 +0300

    1. Split data to train, valid. Processed test data. 2. Feature selection (forward selection using lgbm.feature_importance_.

--- feat_selection.py
+++ feat_selection.py
@@ -1,8 +1,6 @@
 import lightgbm as lgb
-from sklearn.ensemble import ExtraTreesClassifier
 from imblearn.under_sampling import RandomUnderSampler
 
-from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import roc_auc_score
 
@@ -13,65 +11,213 @@ from tqdm import tqdm
 import shap 
 import matplotlib.pyplot as plt
 
+import numpy as np 
+import math
+
 SEED = 32
-#%%
+#%% load filled-nnan data: train, valid, test set
 with open('pkl/df_train_filled.pkl', 'rb') as file:
     data = pkl.load(file)
 
-df = data['df_train']
+df_train = data['df_train']
 feats_num = data['feats_num']
 feats_cat = data['feats_cat']
-X_train, y_train = df.iloc[:, :-1], df.iloc[:, -1]
+X_train, y_train = df_train.iloc[:, :-1], df_train.iloc[:, -1]
+
+# load valid set
+with open('pkl/df_valid_filled.pkl', 'rb') as file:
+    data = pkl.load(file)
+
+df_valid = data['df_valid']
+X_valid, y_valid = df_valid.iloc[:, :-1], df_valid.iloc[:, -1]
+
+# load test set
+with open('pkl/df_test_filled.pkl', 'rb') as file:
+    data = pkl.load(file)
+
+df_test = data['df_test']
+X_test = df_test.copy()
 #%% FEATURE TRANSFORMATION
 print('train transformation')
+# numerical features
+X_num_train = StandardScaler().fit_transform(X_train[feats_num])
+X_num_train = pd.DataFrame(X_num_train, columns=feats_num)
+X_num_valid = StandardScaler().fit_transform(X_valid[feats_num])
+X_num_valid = pd.DataFrame(X_num_valid, columns=feats_num)
+X_num_test = StandardScaler().fit_transform(X_test[feats_num])
+X_num_test = pd.DataFrame(X_num_test, columns=feats_num)
 
-X_num = StandardScaler().fit_transform(X_train[feats_num])
-X_num = pd.DataFrame(X_num, columns=feats_num)
-X_cat = OHE_topN(top_n=10).fit_transform(X_train[feats_cat])
-feats_cat = X_cat.columns
-X_train_clean = pd.concat([X_num, X_cat], axis=1)
+# categorical features
+ohe = OHE_topN(top_n=10)
+ohe.fit(X_train[feats_cat])
+X_cat_train = ohe.fit_transform(X_train[feats_cat])
+X_cat_valid = ohe.transform(X_valid[feats_cat])
+X_cat_test = ohe.transform(X_test[feats_cat])
+feats_cat = X_cat_train.columns
 
-#%% statistic of best models 
-with open('pkl/df_hp.pkl', 'rb') as file:
-    df_hp = pkl.load(file) 
+# concatenatenate numerical and categorical features
+X_clean_train = pd.concat([X_num_train, X_cat_train], axis=1)
+X_clean_valid = pd.concat([X_num_valid, X_cat_valid], axis=1)
+X_clean_test = pd.concat([X_num_test, X_cat_test], axis=1)
+feats = X_clean_train.columns.tolist()
+#%% functions to fit a model
+
+def roc_auc_feval(preds, train_data):
+    ''' 
+    Each evaluation function should accept two parameters: 
+    preds, train_data, and 
+    return (eval_name, eval_result, is_higher_better) 
+    or list of such tuples
+    '''
+    score = roc_auc_score(y_valid, preds)
+    return 'roc_auc_score', score, True 
+
+def fit(X_train, y_train, X_valid=X_clean_valid, y_valid=y_valid, params=None):
     
-with open('pkl/df_res.pkl', 'rb') as file:
-    df_res = pkl.load(file)    
-#%%
-# best model
-idx = df_res.index[0]  
-mask = (df_hp.index == idx)
-params = df_hp.drop('loss', axis=1).iloc[mask, :].T.squeeze().to_dict()
-#%%
+    # undersampling data
+    rus = RandomUnderSampler(random_state=SEED)
+    X_res, y_res = rus.fit_resample(X_train, y_train)
+   
+    # prepare to lgbm fitting
+    train_set = lgb.Dataset(data=X_res, label=y_res, 
+                            feature_name=X_res.columns.tolist())
+    valid_set = lgb.Dataset(data=X_clean_valid, label=y_valid,
+                            feature_name=X_res.columns,
+                            reference=train_set)
+    if not params:
+        params = {'num_leaves': 31, 'force_col_wise': True}
+    
+    # params of the lgbm-model
+    lgbm = lgb.train(params=params, 
+                      train_set=train_set, 
+                      valid_sets=[valid_set],
+                      num_boost_round=100, 
+                      early_stopping_rounds=25,
+                      feval=roc_auc_feval,
+                      verbose_eval=-1)
+    
+    return lgbm
+
+lgbm = fit(X_clean_train, y_train)
+#%% UNDER SAMPLING
 rus = RandomUnderSampler(random_state=SEED)
-X_res, y_res = rus.fit_resample(X_train_clean, y_train)
-
-#%% SHAP
-models = {
-    'lgb_params': lgb.LGBMClassifier(**params, random_state=SEED),
-    'lgb': lgb.LGBMClassifier(random_state=SEED),
+X_res, y_res = rus.fit_resample(X_clean_train, y_train) 
+#%% prepare to lgbm fitting !!!!!!!!!!!!!!!!!
+train_set = lgb.Dataset(data=X_res, label=y_res, 
+                        feature_name=X_res.columns.tolist())
+valid_set = lgb.Dataset(data=X_clean_valid, label=y_valid,
+                        feature_name=X_res.columns,
+                        reference=train_set)
+# params of the lgbm-model
+params = {
+    'num_leaves': 31
     }
-for title, model in models.items():
-    
-    print('fitting', title)
-    model.fit(X_res, y_res)    
-    explainer = shap.TreeExplainer(model).shap_values(X_res)
-    fig = shap.summary_plot(explainer, X_res, feature_names=X_res.columns,
-                      max_display=X_res.shape[1], show=False)                                                  
 
-    plt.savefig(f'media/shap_values_{title}')    
+# fitting the model
+lgbm = lgb.train(params=params, 
+                  train_set=train_set, 
+                  valid_sets=[valid_set],
+                  num_boost_round=100, 
+                  early_stopping_rounds=25,
+                  feval=roc_auc_feval,
+                  verbose_eval=10)
+
+preds = lgbm.predict(X_res)
+score = roc_auc_score(y_res, preds)
+print(score.round(4))
+
+preds = lgbm.predict(X_clean_train)
+score = roc_auc_score(y_train, preds)
+print(score.round(4))
+
+preds = lgbm.predict(X_clean_valid)
+score = roc_auc_score(y_valid, preds)
+print(score.round(4))
+#%% PLOT FEATURE IMPORTANCE
+
+ax = lgb.plot_importance(lgbm, max_num_features=200, 
+                         figsize=(15, 15))
+plt.savefig('media/lgbm.plot_importance.png')
+#%% FEATURE IMPORTANCE TO DATAFRAME. EDA.
+lgbm = fit(X_res, y_res)
+
+df_fi = pd.Series(lgbm.feature_importance())
+df_fi.index = X_res.columns
+df_fi.sort_values(ascending=False, inplace=True)
+
+n = X_clean_train.shape[1]
+ncols = 4
+nrows = math.ceil(n/ncols)
+fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(ncols*3, nrows*3))
+
+for idx, feat in enumerate(df_fi.index[:n]):
+    
+    ax = axes[idx//4, idx%4]
+    
+    X_clean_train.loc[:, feat].hist(ax=ax, color='b', alpha=0.4, label='train')
+    X_clean_valid.loc[:, feat].hist(ax=ax, color='r', alpha=0.5, label='valid')
+    X_clean_test.loc[:, feat].hist(ax=ax, color='g', alpha=0.4, label='test')
+    ax.legend()
+    ax.set_title(feat)
+
+fig.tight_layout()
+fig.savefig('media/distr.hist.train_valid_test.png')
+#%% Sequential forward selection using feature_importance
+
+dct_res, dct_train, dct_valid, dct_test, iters = {}, {}, {}, {}, {}
+n = len(df_fi.index)  # number of feature_importance
+feats_selected = [] # feats that will be used in forward selection
+
+for feat in tqdm(df_fi.index[:n]):
+    
+    feats_selected.append(feat)
+    length = len(feats_selected)
+    
+    lgbm = fit(X_res[feats_selected], y_res)
+    iters[length] = lgbm.best_iteration
+    
+    preds = lgbm.predict(X_res[feats_selected])
+    score = roc_auc_score(y_res, preds)
+    dct_res[length] = score.round(4)
+    
+    preds = lgbm.predict(X_res[feats_selected])
+    score = roc_auc_score(y_res, preds)
+    dct_train[length] = score.round(4)
+    
+    preds = lgbm.predict(X_clean_valid[feats_selected])
+    score = roc_auc_score(y_valid, preds)
+    dct_valid[length] = score.round(4)
+
+dct = {'res': dct_res, 'train': dct_train, 'valid': dct_valid, 'n_estim': iters}
+df_res = pd.DataFrame(dct)
+
+df_res.sort_values(by='valid', ascending=False, inplace=True)
+
+#%% save my clean train, valid and test sets for tuning model
+
+# the best result on valid set
+best = df_res.head(1).index.values[0]
+feats_to_save = df_fi[:best].index 
+
+train_set_clean = {
+    'X_clean_train': X_clean_train[feats_to_save],
+    'y_train': y_train,
+    'feats_to_save': feats_to_save
+    }
+with open('pkl/df_train_clean.pkl', 'wb') as file:
+    pkl.dump(train_set_clean, file)
+    
+valid_set_clean = {
+    'X_valid_clean': X_clean_valid[feats_to_save],
+    'y_valid': y_valid
+    }
+with open('pkl/df_valid_clean.pkl', 'wb') as file:
+    pkl.dump(valid_set_clean, file)    
+
+test_set_clean = {
+    'X_test_clean': X_clean_test[feats_to_save],
+    }
+with open('pkl/df_test_clean.pkl', 'wb') as file:
+    pkl.dump(test_set_clean, file)
 #%%
-model = models['lgb_params']
-imp = pd.Series(model.feature_importances_)
-imp.index = X_res.columns
-imp.sort_values(ascending=False, inplace=True)
-feats_imp = imp.index
-#%% save to testing.py
-with open('pkl/feats_imp.pkl', 'wb') as file:
-    pkl.dump(feats_imp, file)    
-    
-    
-    
-    
-    
-    
+X_res[feats_to_save]
--- feats_mis_vals.py
+++ feats_mis_vals.py
@@ -33,39 +33,46 @@ def plot_relations(df, feats, file_name, ncols=10):
     fig.savefig(f'media/{file_name}')
 #%%
 PATH_DIR = '../../../bases/telecom-clients-prediction2'
-FILE_NAME = 'orange_small_churn_train_data.csv'
-PATH_FILE = os.path.join(PATH_DIR, FILE_NAME)
+FILE_NAME_TRAIN = 'orange_small_churn_train_data.csv'
+FILE_NAME_TEST = 'orange_small_churn_test_data.csv'
+PATH_FILE_TRAIN = os.path.join(PATH_DIR, FILE_NAME_TRAIN)
+PATH_FILE_TEST = os.path.join(PATH_DIR, FILE_NAME_TEST)
 #%%
-df = pd.read_csv(PATH_FILE, index_col='ID')
-feats = df.columns
-feats_num = df.columns[:190]
-feats_cat = df.columns[190:-1]
-trg = df.columns[-1]
-print('shape of raw data:', df.shape)     # (18299, 231)
+df_train = pd.read_csv(PATH_FILE_TRAIN, index_col='ID')
+feats = df_train.columns
+feats_num = df_train.columns[:190]
+feats_cat = df_train.columns[190:-1]
+trg = df_train.columns[-1]
+print('shape of raw train data:', df_train.shape)     # (18299, 231)
+
+df_test = pd.read_csv(PATH_FILE_TEST, index_col='ID')
+print('shape of raw train data:', df_test.shape)     # (18299, 231)
 #%% drop elements where target is nan
-df.drop(np.where(df.iloc[:, -1].isna())[0], axis=0, inplace=True)
-print('shape of nan-target: ', df.shape)  # (18298, 231)
+df_train.drop(np.where(df_train.iloc[:, -1].isna())[0], axis=0, inplace=True)
+print('train: shape of nan-target: ', df_train.shape)  # (18298, 231)
 #%%
-X = df.iloc[:, :-1].values  
-y = df.iloc[:, -1].values
+X_train = df_train.iloc[:, :-1].values  
+y_train = df_train.iloc[:, -1].values
 #%% split data and save test set
-X_train, X_test, y_train, y_test = train_test_split(X, y,                    
+X_train, X_valid, y_train, y_valid = train_test_split(
+                    X_train, y_train,                    
                     test_size=0.25, shuffle=True, 
-                    stratify=y, random_state=SEED)
+                    stratify=y_train, random_state=SEED)
 
-df = pd.DataFrame(np.c_[X_train, y_train], columns=feats)
-df_test = pd.DataFrame(np.c_[X_test, y_test], columns=feats)
+df_train = pd.DataFrame(np.c_[X_train, y_train], columns=feats)
+df_valid = pd.DataFrame(np.c_[X_valid, y_valid], columns=feats)
 
-with open('pkl/df_test.pkl', 'wb') as file:
+with open('pkl/df_valid.pkl', 'wb') as file:
     pkl.dump(df_test, file)
 
-print('shape of df_train:', df.shape)
+print('shape of df_train:', df_train.shape)
+print('shape of df_valid:', df_valid.shape)
 print('shape of df_test:', df_test.shape)
 #%%
-df.iloc[:, -1].value_counts().to_dict() 
+df_train.iloc[:, -1].value_counts().to_dict() 
 #{-1.0: 12690, 1.0: 1033}
 
-(df.iloc[:, -1].value_counts()/len(df)).round(3).to_dict()
+(df_train.iloc[:, -1].value_counts()/len(df_train)).round(3).to_dict()
 # {-1.0: 0.925, 1.0: 0.075}
 #%%
 print('Handling missing values')
@@ -74,14 +81,15 @@ print('Handling missing values')
 # fig = fig.get_figure()
 # fig.savefig('media/msno.png') 
 #%% Remove completly nan columns 
-feats_num = df[feats_num].dropna(how='all', axis=1).columns
-feats_cat = df[feats_cat].dropna(how='all', axis=1).columns
+feats_num = df_train[feats_num].dropna(how='all', axis=1).columns
+feats_cat = df_train[feats_cat].dropna(how='all', axis=1).columns
 #%% HANDLING NUMERICAL MIS. DATA. 
 #%%Drop feats with nan more than 0.925
-mask = (df[feats_num].isna().mean() >= 0.925)
-feats_to_del = df[feats_num].loc[:, mask].columns
+mask = (df_train[feats_num].isna().mean() >= 0.925)
+feats_to_del = df_train[feats_num].loc[:, mask].columns
 feats_num = list(filter(lambda x: x not in feats_to_del, feats_num))
-df.drop(feats_to_del, inplace=True, axis=1)
+df_train.drop(feats_to_del, inplace=True, axis=1)
+df_valid.drop(feats_to_del, inplace=True, axis=1)
 df_test.drop(feats_to_del, inplace=True, axis=1)
 #%% Relationship between MVs and target
 
@@ -89,19 +97,23 @@ df_test.drop(feats_to_del, inplace=True, axis=1)
 #%% KNN Imputation, fit and transform train_data
 
 # imputer = KNNImputer()
-# imputer.fit(df[feats_num])
+# imputer.fit(df_train[feats_num])
+
+# save imputer
+# with open('pkl/imputer_knn.pkl', 'wb') as file:
+#     pkl.dump(imputer, file)
+
 with open('pkl/imputer_knn.pkl', 'rb') as file:
     imputer = pkl.load(file)
 
-df[feats_num] = imputer.transform(df[feats_num])
+df_train[feats_num] = imputer.transform(df_train[feats_num])
+df_valid[feats_num] = imputer.transform(df_valid[feats_num])
 df_test[feats_num] = imputer.transform(df_test[feats_num])
 
 print('\nNumerical data. KNN imputation')
-print('shape of df_train:', df.shape)
+print('shape of df_train:', df_train.shape)
+print('shape of df_valid:', df_valid.shape)
 print('shape of df_test:', df_test.shape)
-
-# with open('pkl/knn_imputer.pkl', 'wb') as file:
-#     pkl.dump(imputer, file)
 #%% HANDLING MISSING VALUES. CATEGORICAL DATA. 
 #%% Relationship between MVs and target
 
@@ -111,11 +123,13 @@ feats_to_del = [191, 201, 213, 215, 224]   # features to delete
 feats_to_del = [f'Var{col}' for col in feats_to_del]
 
 feats_cat = list(filter(lambda x: x not in feats_to_del, feats_cat))
-df.drop(feats_to_del, axis=1, inplace=True)
+df_train.drop(feats_to_del, axis=1, inplace=True)
+df_valid.drop(feats_to_del, axis=1, inplace=True)
 df_test.drop(feats_to_del, axis=1, inplace=True)
 
 print('\nCategorical data. Creating new features replacing nan values with "rare" category')
-print('shape of df_train:', df.shape)
+print('shape of df_train:', df_train.shape)
+print('shape of df_valid:', df_valid.shape)
 print('shape of df_test:', df_test.shape)
 #%% Relationship between MVs and target. Partially deleted feats.
 
@@ -130,81 +144,91 @@ for feat in feats_to_new:
     new = 'isna_' + feat
     feats_cat.append(new)
     
-    df[new] = np.where(df[feat].isna(), 'missing', 'existing')
-    df[feat] = np.where(df[feat].isna(), 'rare', df[feat])
+    df_train[new] = np.where(df_train[feat].isna(), 'missing', 'existing')
+    df_train[feat] = np.where(df_train[feat].isna(), 'rare', df_train[feat])
+    
+    df_valid[new] = np.where(df_valid[feat].isna(), 'missing', 'existing')
+    df_valid[feat] = np.where(df_valid[feat].isna(), 'rare', df_valid[feat])
     
     df_test[new] = np.where(df_test[feat].isna(), 'missing', 'existing')
     df_test[feat] = np.where(df_test[feat].isna(), 'rare', df_test[feat])
 
 print('\nCategorical data. Creating new features replacing nan values with "rare" category')
-print('shape of df_train:', df.shape)
-print('shape of df_test:', df_test.shape)    
-
+print('shape of df_train:', df_train.shape)
+print('shape of df_valid:', df_valid.shape)
+print('shape of df_test:', df_test.shape)
 #%% MCAR: inpute 'missing'
-mask = df[feats_cat].isna().any()
-feats_cat_na = df[feats_cat].columns[mask]    
+mask = df_train[feats_cat].isna().any()
+feats_cat_na = df_train[feats_cat].columns[mask]    
 #%%  Relationship between MVs and target. Partially deleted feats.
 # plot_relations(df, feats_cat_na, ncols=4, file_name='MV_targ3.png') 
 #%% MCAR: most frequent strategy
 
 for feat in feats_cat:
 
-    top = df[feat].value_counts().nlargest(1).index.values[0]
-    df[feat].fillna(top, inplace=True)
+    top = df_train[feat].value_counts().nlargest(1).index.values[0]
+    df_train[feat].fillna(top, inplace=True)
+    df_valid[feat].fillna(top, inplace=True)
     df_test[feat].fillna(top, inplace=True)
 
 print('\nCategorical data. MCAR: most_frequent')
-print('shape of df_train:', df.shape)
-print('shape of df_test:', df_test.shape)        
-#%%
-isna = df[feats_num+feats_cat+ ['labels']].isna().any().any() # False
+print('shape of df_train:', df_train.shape)
+print('shape of df_valid:', df_valid.shape)
+print('shape of df_test:', df_test.shape)      
+#%% check nan
+isna = df_train[feats_num + feats_cat + ['labels']].isna().any().any() # False
 print('train is NaN:', isna)
-isna = df_test[feats_num+feats_cat+ ['labels']].isna().any().any() # False
+isna = df_valid[feats_num + feats_cat + ['labels']].isna().any().any() # False
+print('valid is NaN:', isna)
+isna = df_test[feats_num+feats_cat].isna().any().any() # False
 print('test is NaN:', isna)
-#%%
-df[trg] = df[trg].astype(int)
-df_test[trg] = df_test[trg].astype(int)
-#%% save my train and test set for further processing
+#%% convert target type to int
+df_train[trg] = df_train[trg].astype(int)
+df_valid[trg] = df_valid[trg].astype(int)
+#%% save my train, valid and test sets for further processing
 train_set_filled = {
-    'df_train': df[feats_num+feats_cat+['labels']],
+    'df_train': df_train[feats_num+feats_cat+['labels']],
     'feats_num': feats_num,
     'feats_cat': feats_cat
     }
 with open('pkl/df_train_filled.pkl', 'wb') as file:
     pkl.dump(train_set_filled, file)
+    
+valid_set_filled = {
+    'df_valid': df_valid[feats_num+feats_cat+['labels']],
+    'feats_num': feats_num,
+    'feats_cat': feats_cat
+    }
+with open('pkl/df_valid_filled.pkl', 'wb') as file:
+    pkl.dump(valid_set_filled, file)    
 
 test_set_filled = {
-    'df_test': df[feats_num+feats_cat+['labels']],
+    'df_test': df_test[feats_num+feats_cat],
     'feats_num': feats_num,
     'feats_cat': feats_cat
     }
 with open('pkl/df_test_filled.pkl', 'wb') as file:
     pkl.dump(test_set_filled, file)
+#%%
+import math 
+import matplotlib.pyplot as plt 
 
+n = 10
+ncols = 4
+nrows = math.ceil(n/ncols)
+fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(nrows*3, ncols*3))
 
-
-
-
-
+for idx, feat in enumerate(df_train.columns[:n]):
     
+    ax = axes[idx//4, idx%4]
     
+    df_train.loc[:, feat].hist(ax=ax, color='b', alpha=0.4, label='train')
+    df_valid.loc[:, feat].hist(ax=ax, color='r', alpha=0.5, label='valid')
+    df_test.loc[:, feat].hist(ax=ax, color='g', alpha=0.4, label='test')
+    ax.legend()
+    ax.set_title(feat)
+
+# fig.legend()
+fig.tight_layout()
+fig.savefig('media/distr.hist.train_valid_test.png')    
     
-    
-    
-    
-    
-    
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-
